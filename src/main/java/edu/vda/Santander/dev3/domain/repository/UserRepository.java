f0627bff53d77e837d93d0149f12975c34c4db36
Veronica <almeida.vrc@gmail.com>
2023-09-15 06:57:44 -0300

    Add repository

---
+++ src/main/java/edu/vda/Santander/dev3/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package edu.vda.Santander.dev3.domain.repository;
+
+import edu.vda.Santander.dev3.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
