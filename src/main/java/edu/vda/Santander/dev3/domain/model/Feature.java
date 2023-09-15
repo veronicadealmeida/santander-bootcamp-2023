565b2ccc756e1b8b3c55d61802f31db765e47557
Veronica <almeida.vrc@gmail.com>
2023-09-15 06:43:17 -0300

    Add domain classes

---
+++ src/main/java/edu/vda/Santander/dev3/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package edu.vda.Santander.dev3.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/edu/vda/Santander/dev3/domain/model/News.java
@@ -0,0 +1,8 @@
+package edu.vda.Santander.dev3.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
