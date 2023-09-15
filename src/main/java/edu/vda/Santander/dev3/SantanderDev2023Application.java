1b18d4c4fe8d0d219197a908c278c918fad6bfec
Veronica <almeida.vrc@gmail.com>
2023-09-15 05:50:12 -0300

    first commit

---
+++ src/main/java/edu/vda/Santander/dev3/SantanderDev2023Application.java
@@ -0,0 +1,13 @@
+package edu.vda.Santander.dev3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDev2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDev2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/vda/Santander/dev3/SantanderDev2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vda.Santander.dev3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDev2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
