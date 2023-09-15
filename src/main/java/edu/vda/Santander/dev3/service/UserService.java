41241e6d23dd23504784af0a1bbbecb938c04e7f
Veronica <almeida.vrc@gmail.com>
2023-09-15 08:51:01 -0300

    Cria métodos GetById e Post

--- src/main/java/edu/vda/Santander/dev3/service/UserService.java
+++ src/main/java/edu/vda/Santander/dev3/service/UserService.java
@@ -1,6 +1,7 @@
 package edu.vda.Santander.dev3.service;
 
 import edu.vda.Santander.dev3.domain.model.User;
+import org.springframework.stereotype.Service;
 
 public interface UserService {
 
--- src/main/java/edu/vda/Santander/dev3/service/impl/UserServiceImpl.java
+++ src/main/java/edu/vda/Santander/dev3/service/impl/UserServiceImpl.java
@@ -3,13 +3,16 @@ package edu.vda.Santander.dev3.service.impl;
 import edu.vda.Santander.dev3.domain.model.User;
 import edu.vda.Santander.dev3.domain.repository.UserRepository;
 import edu.vda.Santander.dev3.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    public UserServiceImpl(UserRepository userRepository){
+
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
@@ -20,8 +23,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(User userToCreate) {
-        if ( userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())){
-            throw new IllegalArgumentException("This Account Number already exists.");
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
         }
         return userRepository.save(userToCreate);
     }
